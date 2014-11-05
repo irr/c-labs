4d9af713b8459a6ceecb2e84a86549cfc720859a
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2014-11-05 07:37:37 -0200

    roman

--- c++0x/tests/test.cc
+++ c++0x/tests/test.cc
@@ -1,4 +1,6 @@
+#include <algorithm>
 #include <iostream>
+#include <iomanip>
 #include <map>
 #include <set>
 #include <string>
@@ -57,5 +59,52 @@ int main(int argc, char **argv) {
         }
 
         cout << "\t\tres: y " << ((ok) ? "contains x" : "do not contains x") << "\n\n";
+    }
+
+    // http://www.wikihow.com/Learn-Roman-Numerals
+    // MCMLXXXIV = 1984 (M=1000; CM=900; LXXX=80; IV=4)
+
+    cout << "converting to roman-numerals:\n\n";
+
+    map<int, string> mr = { {1, "I"},    {2, "II"},    {3, "III"}, {4, "IV"}, {5, "V"}, 
+                            {6, "VI"},   {7, "VII"},   {8, "VII"}, {9, "IX"}, {10, "X"},
+                            {20, "XX"},  {30, "XXX"},  {40, "XL"}, {50, "L"}, {60, "LX"},
+                            {70, "LXX"}, {80, "LXXX"}, {90, "XC"}, 
+
+                            {100, "C"},  {200, "CC"},  {300, "CCC"}, {400, "CD"},
+                            {500, "D"},  {600, "DC"},  {700, "DCC"}, {800, "DCCC"}, 
+                            {900, "CM"}, {1000, "M"}};
+
+    vector<unsigned int> vd;
+
+    for(auto it = mr.begin(); it != mr.end(); ++it) {
+        vd.push_back(it->first);
+    }
+    auto vc = [](unsigned int a, unsigned int b){ return a > b; };
+    sort(vd.begin(), vd.end(), vc);
+
+    vector <unsigned int> vn = {1984, 3999};
+    
+    for (unsigned int n : vn) {
+        cout << "\tN=" << n << endl;
+        vector<string> x;
+        unsigned int v = n;
+        for (unsigned int d : vd) {
+            unsigned int a = v / d;
+            for (unsigned int i = 0; i < a; ++i) {
+                x.push_back(mr[d]);
+            }
+            if (a > 0) {
+                v = v - (a * d);
+                cout << "\t\t" << setw(4) << d << ": " << a << " - " << v << endl;
+            }
+        }
+
+        cout << "\tRoman-Numeral: ";
+        for (string a : x) {
+            cout << a;
+        }
+
+        cout << endl << endl;
     }
 }
