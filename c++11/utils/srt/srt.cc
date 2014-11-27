86baf891eae98919b4e1c8151d27489e97a04412
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2014-11-27 10:11:33 -0200

    centos6

--- c++11/utils/srt/srt.cc
+++ c++11/utils/srt/srt.cc
@@ -11,6 +11,22 @@
 
 #include <iconv.h>
 
+#include <features.h>
+#if not __GNUC_PREREQ(4,5)
+const                        // this is a const object...
+class {
+public:
+  template<class T>          // convertible to any type
+    operator T*() const      // of null non-member
+    { return 0; }            // pointer...
+  template<class C, class T> // or any type of null
+    operator T C::*() const  // member pointer...
+    { return 0; }
+private:
+  void operator&() const;    // whose address can't be taken
+} nullptr = {};  
+#endif
+
 // yum install boost-static glibc-static libstdc++-static
 // valgrind --show-reachable=yes --trace-children=yes --leak-check=full ./srt <srt file>
 
