93560ccf41362d790a08b39804d7a6defaf5c2a1
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-09-25 11:23:56 -0300

    signals

--- libtins/sniffer.cc
+++ libtins/sniffer.cc
@@ -6,13 +6,27 @@
 #include <tins/sniffer.h>
 #include <tins/tcp.h>
 
+#include <unistd.h>
+
+#include <csignal>
+#include <cstdlib>
+#include <cstdio>
+
 #include <string>
-#include <stdint.h>
 
 using namespace Tins;
  
 std::ofstream outfile;
- 
+
+void signal_callback_handler(int signum) {
+    printf("Caught signal %02d\n", signum);
+
+    if (signum == SIGINT) {
+        printf("Ctrl-C {%02d} detected. Exiting...\n", signum);
+        exit(0);
+    }
+}
+
 bool stats(TCPStream tcp) { 
     const RawPDU::payload_type& client_payload = tcp.client_payload();
     const RawPDU::payload_type& server_payload = tcp.server_payload();
@@ -21,12 +35,12 @@ bool stats(TCPStream tcp) {
                                           server_payload : client_payload;
 
     TCPStream::StreamInfo info = tcp.stream_info();
-    std::cout << tcp.id() << "," 
-              << info.client_addr << ":" << info.client_port << "<->" 
-              << info.server_addr << ":" << info.server_port << ","
-              << client_payload.size() << ","
-              << server_payload.size() << ","
-              << tcp.is_finished() << std::endl;
+    printf("0x%08lx,%s:%d,%s:%d,%d,%d,%d\n",
+            tcp.id(), 
+            info.client_addr.to_string().c_str(), info.client_port,
+            info.server_addr.to_string().c_str(), info.server_port,
+            client_payload.size(), server_payload.size(),
+            tcp.is_finished());
 
     /*
     std::string tcpstream(payload.begin(),payload.end());
@@ -37,8 +51,13 @@ bool stats(TCPStream tcp) {
 }
  
 int main() {
+    signal(SIGINT, signal_callback_handler);
+    signal(SIGUSR1, signal_callback_handler);
+
     Sniffer sniffer("eth0");
+    
     TCPStreamFollower stalker = TCPStreamFollower();
     stalker.follow_streams(sniffer, stats);
+    
     return 1;
 } 
