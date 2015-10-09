915cfa2eacd212df1d4efde4e4f760513e48e57f
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-10-09 13:28:52 -0300

    json

---
+++ c++11/json/test.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+
+template <typename T>
+
+struct my_id_translator
+{
+    typedef T internal_type;
+    typedef T external_type;
+
+    boost::optional<T> get_value(const T &v) { return  v.substr(1, v.size() - 2) ; }
+    boost::optional<T> put_value(const T &v) { return '"' + v +'"'; }
+};
+
+/*
+{
+    "path1":
+    {
+        "path2":
+        [
+            {
+                "int": "10",
+                "bool": "true"
+            },
+            {
+                "double": "2.2",
+                "string": "\"some string\""
+            }
+        ]
+    }
+}
+ */
+
+int main(int, char *[])
+{
+    using namespace std;
+    using boost::property_tree::ptree;
+    using boost::property_tree::basic_ptree;
+    try {
+        ptree root, arr,elem2;
+        basic_ptree<std::string, std::string> elem1;
+        elem1.put<int>("int", 10 );
+        elem1.put<bool>("bool", true);
+        elem2.put<double>("double", 2.2);
+        elem2.put<std::string>("string", "some string", my_id_translator<std::string>());
+
+        arr.push_back( std::make_pair("", elem1) );
+        arr.push_back( std::make_pair("", elem2) );
+        root.put_child("path1.path2", arr);
+
+        std::stringstream ss;
+        write_json(ss, root);
+
+        std::string my_string_to_send_somewhere_else = ss.str();
+        cout << my_string_to_send_somewhere_else << endl;
+    }
+    catch (std::exception & e) {
+        cout << e.what();
+    }
+    return 0;
+}
+
