90139a840c8f1536e8737f02563f00beb4a391c2
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-10-09 13:32:04 -0300

    simple

--- c++11/json/test.cpp
+++ c++11/json/test.cpp
@@ -5,17 +5,6 @@
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
 
-template <typename T>
-
-struct my_id_translator
-{
-    typedef T internal_type;
-    typedef T external_type;
-
-    boost::optional<T> get_value(const T &v) { return  v.substr(1, v.size() - 2) ; }
-    boost::optional<T> put_value(const T &v) { return '"' + v +'"'; }
-};
-
 /*
 {
     "path1":
@@ -46,7 +35,7 @@ int main(int, char *[])
         elem1.put<int>("int", 10 );
         elem1.put<bool>("bool", true);
         elem2.put<double>("double", 2.2);
-        elem2.put<std::string>("string", "some string", my_id_translator<std::string>());
+        elem2.put<std::string>("string", "some string");
 
         arr.push_back( std::make_pair("", elem1) );
         arr.push_back( std::make_pair("", elem2) );
